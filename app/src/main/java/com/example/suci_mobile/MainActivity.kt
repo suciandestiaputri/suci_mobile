4de427c25230a0a9675fec03e708c0cac9b4816b
suci <suci24ti@mahasiswa.pcr.ac.id>
2026-09-25 10:18:56 +0700

    Penambahan pertemuan 3: Dialog & Pop Up Components MaterialCardView Intent (Kirim data antar Activity)

--- app/src/main/java/com/example/suci_mobile/MainActivity.kt
+++ app/src/main/java/com/example/suci_mobile/MainActivity.kt
@@ -32,7 +32,12 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnToFourth.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("from", "Rumbai")
+            intent.putExtra("age", 25)
             startActivity(intent)
+
+
 
         }
     }
--- app/src/main/java/com/example/suci_mobile/Pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/suci_mobile/Pertemuan_4/FourthActivity.kt
@@ -14,6 +14,8 @@ import com.example.suci_mobile.Pertemuan_3.ThirdResultActivity
 import com.example.suci_mobile.R
 import com.example.suci_mobile.databinding.ActivityFourthBinding
 import com.example.suci_mobile.databinding.ActivityThirdBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 class FourthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFourthBinding
@@ -34,14 +36,39 @@ class FourthActivity : AppCompatActivity() {
 //        val inputNama: EditText = findViewById(R.id.inputNama)
 //        val btnSubmit: Button = findViewById(R.id.btnSubmit)
 
+
+            val name = intent.getStringExtra("name")
+            val from = intent.getStringExtra("from")
+            val age = intent.getIntExtra("age",0)
+            Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+
             binding.button.setOnClickListener {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
                 //Mengambil value dari inputNama dan menampilkan di Logcat
-
-
-
-
+            }
+            binding.btnShowSnackbar.setOnClickListener {
+                Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                    .setAction("Tampilkan") {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        Log.e("Info Snackbar", "Snackbar ditutup")
+                    }
+                    .show()
+            }
+            binding.btnShowAlertDialog.setOnClickListener {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Konfirmasi")
+                    .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                    .setPositiveButton("Ya") { dialog, _ ->
+                        dialog.dismiss()
+                        Log.e("Info Dialog","Anda memilih Ya!")
+                    }
+                    .setNegativeButton("Batal") { dialog, _ ->
+                        dialog.dismiss()
+                        Log.e("Info Dialog","Anda memilih Tidak!")
+                    }
+                    .show()
+            }
         }
     }
-}
